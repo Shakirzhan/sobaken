64c497c31b5a003477c065fa399ed95a366c1e3f
Shakirzhan <samarbaev99@mail.ru>
2021-12-07 09:55:40 +0300

    main page catalog proccess(2)...

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 import 'focus-visible';
 import lazyImages from './modules/lazyImages';
 import documentReady from './helpers/documentReady';
+import $ from "jquery";
 
 documentReady(() => {
   lazyImages();
+
+  $('.catalog-item__button').on('click', () => {
+    
+  })
 });
