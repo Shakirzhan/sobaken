c7dc5639e4fad5d4b45a2e1bc56403a70e7ca6c4
Shakirzhan <samarbaev99@mail.ru>
2021-12-11 18:24:50 +0300

    main page question final...

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,14 @@ documentReady(() => {
       $(".notification").fadeOut('slow')
     }, 500)
   })
+
+  $('.question-button').on('click', e => {
+    const questionItem = $(e.currentTarget).parents('.question-item')
+
+    if(questionItem.hasClass('question-item--open')) {
+      questionItem.removeClass('question-item--open')
+    } else {
+      questionItem.addClass('question-item--open')
+    }
+  })
 });
